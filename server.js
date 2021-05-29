43b023e5857fa26ed059a4ec4709b3930484a8d4
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-05-29 23:58:44 +0530

    http request using express

--- server.js
+++ server.js
@@ -1,52 +1,35 @@
 
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req,res) => {
-    if(req.method === "GET" && req.url === '/hello') {
-        res.write("Hello World from 'GET!'");
-        res.end();
-    }
 
-    if(req.method === "POST" && req.url === '/hello') {
-        res.write("Hello World from 'POST!'");
-        res.end();
-    }
-
-    if(req.url.startsWith('/me')) {
-
-        const urlAfterSlashMe = req.url.substr(3);
-
-        if(urlAfterSlashMe.startsWith('?')) {
-            res.write(urlAfterSlashMe.substr(6));
-            res.end();
-        }
-
-        else if(urlAfterSlashMe.startsWith('/hello')) {
-            const urlAfterSlashHello = urlAfterSlashMe.substr(6);
-            
-            if(urlAfterSlashHello.startsWith('?')) {
-                res.write(`Hello ${urlAfterSlashHello.substr(6)}`);
-                res.end();
-            }
-
-            else if(urlAfterSlashHello.startsWith('/')) {
-                res.write(`Hello ${urlAfterSlashHello.substr(1)}`);
-                res.end();
-            }
-       
-        }
-
-        else {
-            res.write(urlAfterSlashMe.substr(1));
-            res.end();
-        }
-    }
+app.get('/', (req, res) => {
+    res.send('HelloWorld');
 });
 
-// server.on('connection', (socket) => {
-//     console.log('New connection...');
-// });
+app.get('/hello', (req, res) => {
+    res.send("Hello World from 'GET'");
+});
 
-server.listen(7050);
+app.post('/hello', (req, res) => {
+    res.send("Hello World from 'POST'");
+});
 
-console.log('Listening on port 7050...');
+app.get('/me', (req, res) => {
+    res.send(req.query.name);
+});
+
+app.get('/me/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+app.get('/me/hello', (req, res) => {
+    res.send(`Hello ${req.query.name}!`);
+});
+
+app.get('/me/hello/:id', (req, res) => {
+    res.send(`Hello ${req.params.id}!` );
+});
+
+const port = process.env.PORT || 7050;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
